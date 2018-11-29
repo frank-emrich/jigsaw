d8573c0daa33b630cc0fb4801a6399a33ae51946
Frank Emrich <frank.emrich@ed.ac.uk>
2018-11-29 13:07:33 +0000

    updated combined poly variant version

--- experiments/polymoprhic-variants/single-module/combined.ml
+++ experiments/polymoprhic-variants/single-module/combined.ml
@@ -13,15 +13,13 @@ exception TypeError of string
 
 type 'typ core_typ =
   [ `BoolT
-  | `IntT
   | `StringT
   | `Arrow of 'typ * 'typ ]
     [@@deriving show]
 
 
 type ('value, 'term) core_value =
-  [ `IntV of int
-  | `BoolV of bool
+  [ `BoolV of bool
   | `StringV of string
   | `LamV of var * 'term * ('value venv)
   | `RecLamV  of var * var  * 'term * ('value venv) ]
@@ -33,16 +31,13 @@ type ('value, 'term) core_value =
 
 
 and ('term, 'typ) core_term =
-  [ `IntE of int
-  | `BoolE of bool
+  [ `BoolE of bool
   | `StringE of string
   | `LamE of var * 'typ * 'term
   | `RecLamE of var * 'typ * var * 'typ  * 'term
-  | `PlusE of 'term * 'term
   | `VarE of var
   | `AppE of 'term * 'term
-  | `IfE of 'term * 'term * 'term
-  | `IntEq of 'term * 'term ]
+  | `IfE of 'term * 'term * 'term ]
     [@@deriving show]
 
 
@@ -54,7 +49,6 @@ and ('term, 'typ) core_term =
       (term : ('term, 'typ) core_term) : 'typ =
     let tc x = (typecheck env x) in
     match term with
-    | `IntE _ -> `IntT
     | `BoolE _ -> `BoolT
     | `StringE _ ->  `StringT
     | (`LamE (v, vt, b)) ->
@@ -69,10 +63,6 @@ and ('term, 'typ) core_term =
           ft
         else
           raise (TypeError "wrong type annotation in RecLamE")
-    | (`PlusE (l, r)) -> (
-      match (tc l, tc r) with
-      |  `IntT,  `IntT ->  `IntT
-      | _ -> raise (TypeError "adding non-ints") )
     |  (`IfE (cond, t1, t2)) -> (
       match (tc cond, tc t1, tc t2) with
       |  `BoolT, typ1, typ2 when typ1 = typ2 -> typ1
@@ -84,10 +74,7 @@ and ('term, 'typ) core_term =
       | _ -> raise (TypeError "Wrong funcall type") )
     | (`VarE x) ->
       ( try ListLabels.assoc x env with Not_found -> raise (TypeError ("Variable " ^ x ^ "Not found")) )
-    |  (`IntEq (t1, t2) ) -> (
-      match (tc t1, tc t2) with
-      | (`IntT),  `IntT  -> `BoolT
-      | _ -> raise (TypeError "Wrong funcall type") )
+
 
 
 
@@ -98,12 +85,10 @@ and ('term, 'typ) core_term =
       (term : ('term, 'typ) core_term) : 'value =
     let e x = eval env x in
     match term with
-    |  (`IntE i) -> (`IntV i)
     |  (`BoolE b) ->  (`BoolV b)
     |  (`StringE s) -> (`StringV s)
     |  (`LamE (v, _t, b)) -> (`LamV (v,  b, env))
     |  (`RecLamE (f, _rt, v, _vt, b)) ->  (`RecLamV (f, v, b, env))
-    |  (`PlusE (t1, t2)) -> ( match (e t1, e t2) with (`IntV i1), (`IntV i2) ->  (`IntV (i1 + i2)) | _ -> failwith "eval fail" )
     |  (`IfE (c, t1, t2)) -> ( match e c with (`BoolV b) -> if b then e t1 else e t2 | _ -> failwith "eval fail" )
     |  (`AppE (t1, t2)) -> (
       match e t1 with
@@ -116,11 +101,66 @@ and ('term, 'typ) core_term =
           eval env'' b
       | _ -> failwith "eval fail" )
     | (`VarE v) -> List.assoc v env
+
+
+
+
+
+type arith_typ =
+  [ `IntT ]
+    [@@deriving show]
+
+
+type arith_value =
+  [ `IntV of int ]
+    [@@deriving show]
+
+
+type 'term arith_term =
+  [ `IntE of int
+  | `PlusE of 'term * 'term
+  | `IntEq of 'term * 'term ]
+    [@@deriving show]
+
+
+
+
+
+
+
+let arith_typecheck
+      (typecheck )
+      (env : 'typ tenv)
+      (term : ('term)  arith_term) : 'typ =
+  let utc x =  (typecheck env x) in
+  match term with
+    | `IntE _ -> `IntT
+    | (`PlusE (l, r)) -> (
+      match (utc l, utc r) with
+      | `IntT,  `IntT ->  `IntT
+      | _ -> raise (TypeError "adding non-ints") )
+    | (`IntEq (t1, t2) ) -> (
+      match (utc t1, utc t2) with
+      | (`IntT), `IntT  ->  `BoolT
+      | _ -> raise (TypeError "Wrong funcall type") )
+
+
+
+
+let arith_eval
+      (eval)
+      (env : 'value venv)
+      (term : ('term) arith_term ) : 'value =
+    let ue x =  (eval env x) in
+    match term with
+    | (`IntE i) -> (`IntV i)
+    | (`PlusE (t1, t2)) -> ( match (ue t1, ue t2) with  (`IntV i1),  (`IntV i2) -> (`IntV (i1 + i2)) | _ -> failwith "eval fail" )
     | (`IntEq (t1, t2)) ->
-      (match e t1, e t2 with
-        | (`IntV i1),  (`IntV i2) ->
+      (match ue t1, ue t2 with
+        | (`IntV i1), (`IntV i2) ->
           (`BoolV (i1 = i2))
         | _ -> failwith "Evaluation error")
+
 
 
 
@@ -197,6 +237,7 @@ type value = (value, term) pvalue [@@deriving show]
 let rec typecheck (env : typ tenv) (term : term) : typ = match term with
      # core_term as term -> core_typecheck typecheck env term
     |# let_term as term -> let_typecheck typecheck env term
+    |#arith_term as term -> arith_typecheck typecheck env term
 
 
 
@@ -204,6 +245,7 @@ let rec eval (env : value venv) (term: term) : value =
   match term with
     #core_term  as t2 -> core_eval eval env t2
     | #let_term  as t2 -> let_eval eval env t2
+    |#arith_term as t2 -> arith_eval eval env t2
 
 
 
